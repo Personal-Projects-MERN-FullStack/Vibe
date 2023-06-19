318deb513ec833b8003b610b0ec000419809bdaa
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2023-06-20 01:08:44 +0530

    Routes Upadated

--- src/Routes.js
+++ src/Routes.js
@@ -12,12 +12,14 @@ import ProductDetails from "./pages/ProductDetails";
 import Notification from "./components/common/Notification";
 import Login from "./components/Auth/Login";
 import NotLoggedInPage from "./components/common/NotLoggedInPage";
+import OrderPlaced from "./pages/OrderPlaced";
 function AppRoutes() {
   const cartstate = useSelector((state) => state.ui.showcart);
   const loginstate = useSelector((state) => state.ui.showlogin);
   const notification_status = useSelector((state) => state.ui.shownotification);
   const auth = useSelector((state) => state.auth.auth);
   const cartlength = useSelector((state) => state.product.cart.length);
+  const orderplaced = useSelector(state=>state.ui.orderplaced)
   const routes = useRoutes([
     {
       path: "*",
@@ -28,7 +30,9 @@ function AppRoutes() {
       element: (
         <div>
           <Navbar />
-          {notification_status.active && <Notification ninfo={notification_status} />}
+          {notification_status.active && (
+            <Notification ninfo={notification_status} />
+          )}
           {loginstate && <Login />}
           {auth && cartstate && <Sidecart />}
           <Outlet />
@@ -46,35 +50,45 @@ function AppRoutes() {
       element: (
         <div>
           <Navbar />
-          {notification_status.active && <Notification ninfo={notification_status} />}
+          {notification_status.active && (
+            <Notification ninfo={notification_status} />
+          )}
           {auth && <Outlet />}
           {auth && cartstate && <Sidecart />}
           {!auth && <NotLoggedInPage state="login" />}
           <Footer />
         </div>
       ),
-      children: [
-        { path: "/cart", element: <Cart /> },
-      ],
+      children: [{ path: "/cart", element: <Cart /> }],
     },
     {
       path: "/checkout",
       element: (
         <div>
           <Navbar />
-          {notification_status.active && <Notification ninfo={notification_status} />}
+          {notification_status.active && (
+            <Notification ninfo={notification_status} />
+          )}
           {auth && cartstate && <Sidecart />}
           {auth && cartlength > 0 && <Outlet />}
           {auth && cartlength === 0 && <NotLoggedInPage state="outofcart" />}
           <Footer />
         </div>
       ),
-      children: [
-        { path: "/checkout", element: <Checkout /> },
-      ],
+      children: [{ path: "/checkout", element: <Checkout /> }],
+    },
+    {
+      path: "/OrderPlaced",
+      element: (
+        <div>
+          {orderplaced && <Outlet />}
+          {!orderplaced && <Notfound />}
+        </div>
+      ),
+      children: [{ path: "/OrderPlaced", element: <OrderPlaced /> }],
     },
   ]);
-  
+
   return routes;
 }
 
